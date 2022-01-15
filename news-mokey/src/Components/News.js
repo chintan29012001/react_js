3410ad8d9cd436b66184d2e679e910ba43993dcc
Chintan Puggalok <chintan19239@iiitd.ac.in>
2022-01-15 21:11:10 +0530

    added author time and news Source

--- news-mokey/src/Components/News.js
+++ news-mokey/src/Components/News.js
@@ -1,83 +1,132 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
-import Spinner from './Spinner';
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
-    articles=[]
-    
-    async componentDidMount()
-    {
-        this.setState({loading:true});
-        let news_api=await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7a3c2b747e744da78b5bd14e40cfa428&page=${this.state.page}&pageSize=${this.props.itemsPerPage}`);
-        
-        let news_response=await news_api.json()
-        console.log(news_response);
-        this.setState({articles:news_response.articles,totalArticles:news_response.totalResults,loading:false});
-    }
-    constructor()
-    {
-        super();
-        this.state={
-            articles:this.articles,
-            loading:false,
-            page:1,
-            totalArticles:0,
-            
-        }
-    }
-    handleNextClick= async ()=>
-    {
-        let totalPages=Math.ceil(this.state.totalArticles/this.props.itemsPerPage)
-        console.log(totalPages);
-        if(this.state.page+1<=totalPages)
-        {
-            this.setState({loading:true});
-            let news_api=await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7a3c2b747e744da78b5bd14e40cfa428&page=${this.state.page+1}&pageSize=${this.props.itemsPerPage}`);
-            console.log("ne");
-            let news_response=await news_api.json()
-            console.log(news_response);
-            // this.setState({ page: this.state.page + 1 })
-            this.setState({articles:news_response.articles,page: this.state.page + 1,loading:false});
-        }
-    }
-    handlePrevClick=async ()=>
-    {
-        if(this.state.page-1>=1)
-        {
-            this.setState({loading:true});
-            let news_api=await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7a3c2b747e744da78b5bd14e40cfa428&page=${this.state.page-1}&pageSize=${this.props.itemsPerPage}`);
-            
-            let news_response=await news_api.json()
-            // console.log(news_response);
-            // this.setState({ page: this.state.page + 1 })
-            this.setState({articles:news_response.articles,page: this.state.page - 1,loading:false});
-        }
-        
-    }
+  articles = [];
 
-    render() {
-        return (
-            <div className='container text-center' >
-                <h1>Top Headlines</h1>
-                {this.state.loading && <Spinner />}
-                {!this.state.loading &&<div className="row">
-                    {
-                        this.state.articles.map((element)=>{
-                            return <div className="col-md-4" key={element.url}>
-                                <Newsitem title={element.title} description={element.description} image={element.urlToImage} storyUrl={element.url} />
-                            </div>
-                        })
-                    }
-                </div>
-                }
-                <div className="container d-flex justify-content-evenly my-2">
-                <button type="button" disabled={this.state.page<2} onClick={this.handlePrevClick} className="btn btn-dark btn-lg">&larr; Prev</button>
-                <button type="button" disabled={this.state.page===Math.ceil(this.state.totalArticles/this.props.itemsPerPage)} onClick={this.handleNextClick} className="btn btn-dark btn-lg">&rarr; Next</button>
-                    
-                </div>
-            </div>
-        )
+  async componentDidMount() {
+    this.setState({ loading: true });
+    let news_api = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7a3c2b747e744da78b5bd14e40cfa428&page=${this.state.page}&pageSize=${this.props.itemsPerPage}`
+    );
+
+    let news_response = await news_api.json();
+    console.log(news_response);
+    this.setState({
+      articles: news_response.articles,
+      totalArticles: news_response.totalResults,
+      loading: false,
+    });
+  }
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false,
+      page: 1,
+      totalArticles: 0,
+    };
+  }
+  handleNextClick = async () => {
+    let totalPages = Math.ceil(
+      this.state.totalArticles / this.props.itemsPerPage
+    );
+    console.log(totalPages);
+    if (this.state.page + 1 <= totalPages) {
+      this.setState({ loading: true });
+      let news_api = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=${
+          this.props.country
+        }&category=${
+          this.props.category
+        }&apiKey=7a3c2b747e744da78b5bd14e40cfa428&page=${
+          this.state.page + 1
+        }&pageSize=${this.props.itemsPerPage}`
+      );
+      console.log("ne");
+      let news_response = await news_api.json();
+      console.log(news_response);
+      // this.setState({ page: this.state.page + 1 })
+      this.setState({
+        articles: news_response.articles,
+        page: this.state.page + 1,
+        loading: false,
+      });
     }
+  };
+  handlePrevClick = async () => {
+    if (this.state.page - 1 >= 1) {
+      this.setState({ loading: true });
+      let news_api = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=${
+          this.props.country
+        }&category=${
+          this.props.category
+        }&apiKey=7a3c2b747e744da78b5bd14e40cfa428&page=${
+          this.state.page - 1
+        }&pageSize=${this.props.itemsPerPage}`
+      );
+
+      let news_response = await news_api.json();
+      // console.log(news_response);
+      // this.setState({ page: this.state.page + 1 })
+      this.setState({
+        articles: news_response.articles,
+        page: this.state.page - 1,
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container text-center">
+        <h1>Top Headlines - {this.props.category}</h1>
+        {this.state.loading && <Spinner />}
+        {!this.state.loading && (
+          <div className="row">
+            {this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <Newsitem
+                    title={element.title}
+                    description={element.description}
+                    image={element.urlToImage}
+                    storyUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        )}
+        <div className="container d-flex justify-content-evenly my-2">
+          <button
+            type="button"
+            disabled={this.state.page < 2}
+            onClick={this.handlePrevClick}
+            className="btn btn-dark btn-lg"
+          >
+            &larr; Prev
+          </button>
+          <button
+            type="button"
+            disabled={
+              this.state.page ===
+              Math.ceil(this.state.totalArticles / this.props.itemsPerPage)
+            }
+            onClick={this.handleNextClick}
+            className="btn btn-dark btn-lg"
+          >
+            &rarr; Next
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default News
+export default News;
--- news-mokey/src/Components/Newsitem.js
+++ news-mokey/src/Components/Newsitem.js
@@ -1,31 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
-    short(title,len) {
-        return title?title.length<len?title:title.slice(0,len):"demo"
-    };
-    render() {
-        let {title,description,image,storyUrl}=this.props;
-        return (
-                
-                    <div className="card d-md-flex flex-column align-items-center " style={{margin:"2%"}}>
-                        <div className="card d-flex flex-direction-column align-items-center " style={{height:"40%",maxWidth:"90%",margin:"3%",padding:"1%"}}>
-                            <img src={image?image:"https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/333200/333221.6.jpg"} className="card-img-top" style={{maxWidth:"100%",maxHeight:"100%"}} alt="..." />
-                        </div>
-                        <div className='flex-fill d-flex flex-column align-items-center' >
-                            <div className="card-body d-flex flex-column align-items-center flex-grow-1" style={{textAlign:"center"}} >
-                                <h5 className="card-title">{this.short(title,50)}</h5>
-                                <p className="card-text flex-grow-1">{this.short(description,70)}</p>
-                            </div>
-                            <div className="card-body flex-column flex-justify-content-center mt-auto" >
-                                <a href={storyUrl} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                
+    extracter(text)
+    {
+        let x=text.indexOf("-")
+        if(x!=-1)
+        {
+            return text.slice(0,x)
+        }
+        return text
 
-        )
     }
+
+  nullCheck(title) {
+      
+    return title ? title : "demo";
+  }
+  render() {
+    let { title, description, image, storyUrl, author,date,source } = this.props;
+    return (
+      <div
+        className="card d-md-flex flex-column align-items-center "
+        style={{ margin: "2%" }}
+      >
+          
+        <div
+          className="d-flex flex-direction-column align-items-center "
+          style={{
+            height: "40%",
+            maxWidth: "90%",
+            margin: "3%",
+            padding: "1%",
+          }}
+        >
+            
+          <img
+            src={
+              image
+                ? image
+                : "https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/333200/333221.6.jpg"
+            }
+            className="card-img-top"
+            style={{ maxWidth: "100%", maxHeight: "100%" }}
+            alt="..."
+          />
+        </div>
+        <div className="flex-fill d-flex flex-column align-items-center">
+          <div
+            className="card-body d-flex flex-column align-items-center flex-grow-1"
+            style={{ textAlign: "center" }}
+          >
+            <h5 className="card-title">{this.extracter(this.nullCheck(title))} <span className="badge rounded-pill bg-dark">{source}</span> </h5>
+            
+            <p className="card-text flex-grow-1">{this.nullCheck(description)}</p>
+            <p className="card-text"><small className="text-muted">By {author?author:"unknown"} - {new Date(date).toLocaleString()}</small></p>
+          </div>
+          <div className="card-body flex-column flex-justify-content-center mt-auto">
+            <a
+              href={storyUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary btn-sm"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Newsitem
+export default Newsitem;
